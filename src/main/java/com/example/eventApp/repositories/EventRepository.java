95a2baffc880e6fb2b07e9aa4cf6074bfb8e1bec
Andrii Tkachenko <andrii.tkachenko@envelo.pl>
2022-04-13 01:42:43 +0200

    dodane walidacje i endpoint na zapisanie sie do listy

--- src/main/java/com/example/eventApp/repositories/EventRepository.java
+++ src/main/java/com/example/eventApp/repositories/EventRepository.java
@@ -1,6 +1,7 @@
 package com.example.eventApp.repositories;
 
 import com.example.eventApp.model.entity.Event;
+import com.example.eventApp.model.enums.EventStatus;
 import com.example.eventApp.model.enums.EventType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,6 +12,8 @@ import java.util.List;
 public interface EventRepository extends JpaRepository<Event, Long> {
 
     List<Event> findAllByEventType(EventType eventType);
+
+    List<Event> findAllByEventStatus(EventStatus eventStatus);
 
     List<Event> findEventByAuthor_Id(Long id);
 }
--- src/main/java/com/example/eventApp/service/EventService.java
+++ src/main/java/com/example/eventApp/service/EventService.java
@@ -28,24 +28,20 @@ public class EventService {
     private final ModelMapper modelMapper;
 
     public List<EventDTO> getEventsByEventType(EventType eventType) {
-        List<Event> events = eventRepository.findAllByEventType(eventType);
-        return events.stream().map(event -> modelMapper.map(event, EventDTO.class)).collect(Collectors.toList());
+        List<Event> events = getPlannedEventsOnly(eventRepository.findAllByEventType(eventType));
+        return events.stream().map(event -> modelMapper.map(event, EventDTO.class))
+                .collect(Collectors.toList());
     }
 
     public List<EventDTO> getAllUserEvents(Long id) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (!(authentication instanceof AnonymousAuthenticationToken)) {
-            String currentUserName = authentication.getName();
-            User currentUser = userRepository.findByUsername(currentUserName);
-
-        }
-        List<Event> events = eventRepository.findEventByAuthor_Id(id);
-        return events.stream().map(event -> modelMapper.map(event, EventDTO.class)).collect(Collectors.toList());
+        return getPlannedEventsOnly(eventRepository.findEventByAuthor_Id(id))
+                .stream().map(event -> modelMapper.map(event, EventDTO.class))
+                .collect(Collectors.toList());
     }
 
     public EventDTO getEventById(Long id) {
         List<Event> events = eventRepository.findAll();
-        for (Event e: events
+        for (Event e : events
         ) {
             if (e.getId() == id) {
                 return modelMapper.map(e, EventDTO.class);
@@ -55,21 +51,22 @@ public class EventService {
     }
 
     public List<EventDTO> getEventsWithUserInRegistrants(Long id) {
-        List<Event> events = eventRepository.findAll();
+        List<Event> events = eventRepository.findAllByEventStatus(EventStatus.PLANNED);
         User user = userRepository.getById(id);
         List<Event> filtrated = new ArrayList<>();
         for (Event e : events) {
-            if (e.getRegistrants().contains(user) && e.getEventStatus().equals(EventStatus.PLANNED)) {
+            if (e.getRegistrants().contains(user)) {
                 filtrated.add(e);
             }
         }
-        return filtrated.stream().map(event -> modelMapper.map(event, EventDTO.class)).collect(Collectors.toList());
+        return filtrated.stream().map(event -> modelMapper.map(event, EventDTO.class))
+                .collect(Collectors.toList());
     }
 
     public boolean cancelEvent(Long id) {
         Optional<Event> event = eventRepository.findById(id);
 
-        if (event.isPresent()) {
+        if (event.isPresent()&&event.get().getEventStatus().equals(EventStatus.PLANNED)) {
             event.get().setEventStatus(EventStatus.CANCELLED);
             eventRepository.save(event.get());
             return true;
@@ -82,5 +79,14 @@ public class EventService {
         eventRepository.save(modelMapper.map(eventDTO, Event.class));
     }
 
-
+    private List<Event> getPlannedEventsOnly(List<Event> eventsForCheck) {
+        List<Event> events = new ArrayList<>();
+        for (Event e:eventsForCheck
+        ) {
+            if (e.getEventStatus().equals(EventStatus.PLANNED)) {
+                events.add(e);
+            }
+        }
+        return events;
+    }
 }

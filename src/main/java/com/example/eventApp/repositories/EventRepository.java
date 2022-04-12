e24a933664b1c92788c9b96a9e1cf76a0ae6e6ef
Andrii Tkachenko <andrii.tkachenko@envelo.pl>
2022-04-12 17:49:42 +0200

    finaly works

--- src/main/java/com/example/eventApp/repositories/EventRepository.java
+++ src/main/java/com/example/eventApp/repositories/EventRepository.java
@@ -13,6 +13,4 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findAllByEventType(EventType eventType);
 
     List<Event> findEventByAuthor_Id(Long id);
-
-    List<Event> findAllByEventRegistrantsUserId(Long id);
 }
